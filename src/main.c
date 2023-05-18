f385c293b438dc5cef2c5e30704c680cac8a1b6b
Thales Menezes <thlmenezes.dev@gmail.com>
2023-05-18 14:36:15 -0300

    refactor: LSP format

--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
 #include "array.h"
+#include <stdio.h>
 
 /**
  * This is the main function
- * 
+ *
  */
-int main(){
-  Array* list = array_create(5);
+int main() {
+  Array *list = array_create(5);
   printf("%d\n", array_length(list));
-  Array* clone = array_clone(list);
+  Array *clone = array_clone(list);
   array_free(list);
   printf("%d\n", array_length(clone));
   array_free(clone);
